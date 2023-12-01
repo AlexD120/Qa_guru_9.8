654f478701ee6ed48c8728f8e89629805be7cb53
AlexDavydov <AlexDavydov92@gmail.com>
2023-12-01 16:49:13 +0300

    1

---
+++ test_users_simple.py
@@ -0,0 +1,11 @@
+import csv
+
+def test_workers_are_adults():
+
+    with open("users.csv") as f:
+        users = csv.DictReader(f, delimiter=";")
+        workers = [user for user in users if user["status"] == "worker"] # - возьми каждого пользователя из списка пользователей со статусом worker и положи в user
+
+        for worker in workers:
+            assert int(worker["age"]) >= 18, f"Worker {worker['name']} младше 28 лет"
+
